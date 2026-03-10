d374cdb606307a60401adc3e68aa8a56db404777
Maneka Wijesundara <maneka611997@gmail.com>
2026-03-10 10:56:53 +0530

    removed server file

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./api/register") // your register endpoint file
-
-const PORT = 3000
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`)
-})
